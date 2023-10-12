890ee9b8e180192614edc3301e321aea0a5a7435
daviekish <davie.kamweru5612@gmail.com>
2023-10-12 10:40:58 +0300

    Initial Commit

---
+++ Merge Sort/Merge Sort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge_Sort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dictionary<string, object>> data = new List<Dictionary<string, object>>
+            {
+                new Dictionary<string, object> { { "ID", 6000001 }, { "Name", "David Njoroge" },    { "Gender", "Male" },   { "Age", 45 } },
+                new Dictionary<string, object> { { "ID", 6000002 }, { "Name", "Margaret Njoroge" }, { "Gender", "Female" }, { "Age", 35 } },
+                new Dictionary<string, object> { { "ID", 6000011 }, { "Name", "Rawlings Mayan" },   { "Gender", "Male" },   { "Age", 25 } }
+            };
+
+            data = data.OrderBy(item => (int)item["ID"]).ToList();
+            Console.WriteLine("ID\tName\t\t\tGender\tAge");
+
+
+            foreach (var item in data)
+            {
+                Console.WriteLine($"{item["ID"]}\t{item["Name"]}\t{item["Gender"]}\t{item["Age"]}");
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+
+}
